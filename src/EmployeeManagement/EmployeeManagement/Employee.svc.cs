5c9e09a8eb2efb9c49bf7446a228dc9a83a40a56
Vaibhav Goyal <goyalvaibhav@outlook.com>
2021-06-07 02:00:36 +0530

    Use MySQL.Data to fetch query results

---
+++ src/EmployeeManagement/EmployeeManagement/Employee.svc.cs
@@ -0,0 +1,56 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace EmployeeManagement
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Employee" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Employee.svc or Employee.svc.cs at the Solution Explorer and start debugging.
+    public class Employee : IEmployee
+    {
+        private readonly string _connectionString;
+        private const string _query = 
+@"select year(hire_date) as Year, 
+count(*) as TotalHires 
+from employees.employees where @p0 = @p0
+group by year(hire_date);";
+
+        public Employee()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["MySQL_CS"].ConnectionString;
+        }
+
+        public List<GetHiresByYearResult> GetHiresByYear()
+        {
+            var result = new List<GetHiresByYearResult>();
+            using (var con = new MySqlConnection(_connectionString))
+            {
+                con.Open();
+                using (var cmd = new MySqlCommand(_query, con))
+                {
+                    cmd.EnableCaching = false;
+                    cmd.Parameters.AddWithValue("p0", GetRandomNumber());
+                    using (var rdr = cmd.ExecuteReader())
+                    {
+                        if (rdr.HasRows)
+                        {
+                            while (rdr.Read())
+                            {
+                                result.Add(new GetHiresByYearResult { Year = rdr.GetInt32(0), TotalHires = rdr.GetInt64(1) });
+                            }
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private int GetRandomNumber()
+        {
+            var rnd = new Random();
+            return rnd.Next(1, int.MaxValue);
+        }
+    }
+}
---
+++ src/EmployeeManagement/EmployeeManagement/IEmployee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace EmployeeManagement
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IEmployee" in both code and config file together.
+    [ServiceContract]
+    public interface IEmployee
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "api/v1/GetHiresByYear", 
+            RequestFormat = WebMessageFormat.Json, 
+            ResponseFormat = WebMessageFormat.Json)]
+        List<GetHiresByYearResult> GetHiresByYear();
+    }
+
+    [DataContract]
+    public class GetHiresByYearResult
+    {
+        [DataMember]
+        public int Year { get; set; }
+
+        [DataMember]
+        public long TotalHires { get; set; }
+    }
+}
